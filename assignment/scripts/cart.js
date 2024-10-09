dfe9496f473e902d94fb1af0ee764c03bc62e55f
Lamar Pringle <pringlez@ymail.com>
2024-10-09 10:43:41 -0500

    completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,23 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
+function addItem(item) {
+  basket.push(item); 
+  return true;      
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
 
+function listItems() {
+  basket.forEach(item => console.log(item));
+}
 
-
-
-
-
-
+function empty() {
+  basket.length = 0;
+}
 
 
 // DO NOT MODIFY
